62fef4c4d3cec2576c8f54639ce09cb7d35dea2f
jackNEss <jackness1208@qq.com>
2015-09-02 01:04:49 +0800

    [EDIT]单元测试编写

--- unit/init.js
+++ unit/init.js
@@ -2,25 +2,28 @@
 
 var document = window.document,
     testCase = {
-        'mod.box':[
+        'mod.box': [
             {
                 tpl:[
-                    '<div id="modBoxTest">',
-                        '表单内容'
+                    '<div id="modBoxTest" mod-title="标题文字" mod-title-left="左侧标题" mod-title-right="<a href=\'javascript:;\' class=\'mod-btn\'>右侧按钮</a>">',
+                        '表单内容',
                     '</div>'
                 ].join(''),
                 argv: [
                     '#testEl'
-                ],
-                ready: function(){
-
-                }
+                ]
             }
         ] 
     };
 
 var testEl = $('#testEl')[0];
 QUnit.test('mod.box() 模块测试', function(res){
+    var iCase = testCase['mod.box'];
+    for(var i = 0, iModule, len = iCase.length; i < len; i++){
+        iModule = iCase[i];
+        testEl.innerHTML = iModule.tpl;
+        res.ok(mod.box.apply(mod, iModule.argv), 'Passed');
+    }
     res.ok(mod.box('#testEl'), 'passed');
     res.ok(bb('#testEl'), 'passed');
 });
